106989d3f4b48fab0b242cfca21a49197cc13f91
Maxime <maxzeel@MacBookPro.ht.home>
2026-08-28 16:18:32 -0200

    Final clean env config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pkaolympjftqfgvmhjvk.supabase.co'
-
-const part1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrYW9ieW1wamZ0cWZndm1oanZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NzI4OTQsImV4cCI6MjEwMzM0ODg5NH0.'
-const part2 = 'HsJfbLEXZ2-0Qc9Hn1S21GiyXz2CN4U-Eo6g9He4LSw'
-const supabaseAnonKey = part1 + part2
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://pkaolympjftqfgvmhjvk.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrYW9ieW1wamZ0cWZndm1oanZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NzI4OTQsImV4cCI6MjEwMzM0ODg5NH0.HsJfbLEXZ2-0Qc9Hn1S21GiyXz2CN4U-Eo6g9He4LSw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
